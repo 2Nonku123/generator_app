4e8338ebca2a5fab08f209bab2dd372d408485eb
simelane-jpd <201575227@student.uj.ac.za>
2022-07-25 11:23:30 +0200

    deleted duplicate database file

--- server/database/new.sql
+++
@@ -1,6 +0,0 @@
-create database IF NOT EXISTS generator_app;
-create role IF NOT EXISTS generator_user login password 'generator123';
-grant all privileges on database generator_app to generator_user;
-
-
-
